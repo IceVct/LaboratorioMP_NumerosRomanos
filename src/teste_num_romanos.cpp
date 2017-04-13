62393aed2612a33d1544941f002a9be9f8c6030d
icevct <icevct@gmail.com>
2017-04-12 21:18:01 -0300

    mais testes adicionados e mais bem elaborados, todos passaram

--- src/teste_num_romanos.cpp
+++ src/teste_num_romanos.cpp
@@ -1,8 +1,10 @@
 #include "num_romanos.hpp"
 #include "../include/catch.hpp"
 
+//Test case vai testar as duas funcoes implementadas no programa
 TEST_CASE("Numeros romanos para arabicos", "[romanos_arabicos]"){
 
+	//essa secao testa a funcao ValorDecimalAlgRomano
 	SECTION("Testando a funcao que retorna o algarismo arabico correspondente"){
 		REQUIRE(ValorDecimalAlgRomano('I') == 1);
 		REQUIRE(ValorDecimalAlgRomano('V') == 5);
@@ -16,11 +18,28 @@ TEST_CASE("Numeros romanos para arabicos", "[romanos_arabicos]"){
 		REQUIRE(ValorDecimalAlgRomano('i') == -1);
 	}
 
+	//essa secao testa a funcao ConverteNumeroRomano
+	// ERRO vale -1
 	SECTION("Testando a funcao que retorna a conversao de romano para arabico"){
 		REQUIRE(ConverteNumeroRomano("I") == 1);
 		REQUIRE(ConverteNumeroRomano("IV") == 4);
+		REQUIRE(ConverteNumeroRomano("X") == 10);
+		REQUIRE(ConverteNumeroRomano("XIX") == 19);
+		REQUIRE(ConverteNumeroRomano("XLIX") == 49);
+		REQUIRE(ConverteNumeroRomano("CCCLXXXVIII") == 388);
+		REQUIRE(ConverteNumeroRomano("CDLXXIV") == 474);
+		REQUIRE(ConverteNumeroRomano("CMVIII") == 908);
+		REQUIRE(ConverteNumeroRomano("MCDXXXVIII") == 1438);
+		REQUIRE(ConverteNumeroRomano("MDCCCLVII") == 1857);
+		REQUIRE(ConverteNumeroRomano("MMCCLXVI") == 2266);
+		REQUIRE(ConverteNumeroRomano("MMDCCCXCIX") == 2899);
 		REQUIRE(ConverteNumeroRomano("MMCMXCVIII") == 2998);
-		REQUIRE(ConverteNumeroRomano("IiV") == -1);
-		REQUIRE(ConverteNumeroRomano("MMMI") == -1);
+		REQUIRE(ConverteNumeroRomano("MMM") == 3000);
+		REQUIRE(ConverteNumeroRomano("MMvS") == ERRO);
+		REQUIRE(ConverteNumeroRomano("IiV") == ERRO);
+		REQUIRE(ConverteNumeroRomano("DL2") == ERRO);
+		REQUIRE(ConverteNumeroRomano("MMZMx") == ERRO);
+		REQUIRE(ConverteNumeroRomano("MMMMM") == ERRO);
+		REQUIRE(ConverteNumeroRomano("MMMI") == ERRO);
 	}
 }
